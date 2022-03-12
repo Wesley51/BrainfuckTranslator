28eaf5294c2776fb50929feeb731364262a1f870
WesleyTrain <59321467+Wesley51@users.noreply.github.com>
2022-03-12 17:16:27 -0500

    Initial commit

---
+++ brainfucktranslator.py
@@ -0,0 +1,15 @@
+ascii = ['NULL', 'SOH', 'STX', 'ETX', 'EOT', 'ENQ', 'ACK', 'BEL', 'BS', 'HT', 'LF', 'VT', 'FF', 'CR', 'SO', 'SI', 'DLE', 'DC1', 'DC2', 'DC3', 'DC4', 'NAK', 'SYN', 'ETB', 'CAN', 'EM', 'SUB', 'ESC', 'FS', 'GS', 'RS', 'US', 'DEL', ' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+
+brainfuck = ""
+input = input("Enter shit bitch: ")
+for x in input:
+  y = 0
+  for i in ascii:
+    if i == x:
+      y = y - 1
+      for p in range(y):
+        brainfuck = brainfuck + "+"
+      brainfuck = brainfuck + ".>"
+    else:
+      y = y + 1
+print(brainfuck)
